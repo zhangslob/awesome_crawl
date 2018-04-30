465836906b3a1e4a12657070c28cdd83089b7972
zhangslob <zhangslob@gmail.com>
2018-04-30 12:44:48 +0800

    feat: use demjson to fix it

--- qq_news/qq_news/qq_news/spiders/news.py
+++ qq_news/qq_news/qq_news/spiders/news.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import re
+import json
 import scrapy
 
 
@@ -9,14 +11,21 @@ class ExampleSpider(scrapy.Spider):
     allowed_domains = ['qq.com']
     start_urls = ['http://www.qq.com/map/']
 
+    mobile_url = 'https://xw.qq.com/cmsid/{}'
     recommend_url = 'https://pacaio.match.qq.com/xw/relate?num=6&id=20180425A0VYZO&callback=__jp1'  # 推荐新闻
-    # https://pacaio.match.qq.com/irs/rcd?cid=35&token=4d1ddbd5ea23e1ec0cdd44a6ebe8dec6&num=20&page=2&expIds=20180425A1EV59%7C20180425A1811E%7Ci0637cpjxs7%7C20180425A17Q4M%7C20180425A16TS3%7C20180425A16CVB%7C20180425A169J3%7C20180425A15JYI%7C20180425A14YTW%7C20180425A13VV0%7Co0564gbiu9b%7Cq0637sx6bch%7C20180425A0YCYE%7C20180425A0UNVM%7C20180425A0PLBZ%7C20180425A19G4O%7C20180425A1RKMV%7Cr0637ypfft7%7C20180425A0ZM3B%7C20180425A1ODQV&callback=__jp3
+    mobile_ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38' \
+                ' (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
 
     custom_settings = {
         'CONCURRENT_REQUESTS': 64,
         'DOWNLOAD_DELAY': 0,
         'COOKIES_ENABLED': False,
-
+        'DEFAULT_REQUEST_HEADERS': {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'cache-control': 'max-age=0',
+        }
     }
 
     def parse(self, response):
@@ -34,19 +43,30 @@ class ExampleSpider(scrapy.Spider):
 
     def parse_url(self, response):
         """
-        去子分类下采集所有符合要求的详情链接
+        去子分类下采集所有符合要求的详情链接，从移动端爬数据
         :param response: http://news.qq.com/
         :return:
         """
         pat = re.compile('http://new.qq.com/.*/.*.html')
         detail_urls = pat.findall(response.text)
         for url in detail_urls:
-            yield scrapy.Request(url, callback=self.parse_content, headers={'ua': 'mbua'})  # TODO FIX IT
+            url_ = self.mobile_url.format(url.split('/')[-1].split('.')[0])
+            yield scrapy.Request(url_, callback=self.parse_content,
+                                 headers={'User-Agent': self.mobile_ua, 'Referer': url},)
 
     def parse_content(self, response):
         """
         采集详情页数据，从移动端采集会比较简单
-        :param response: 
+        :param response: https://xw.qq.com/cmsid/20180426A0XFTS
         :return: 
         """
-        pass
+        try:   # TODO FIX IT
+            data = ''.join(re.findall(r'globalConfig\s=\s(\{.*?\});', response.text, re.S))
+            title = ''.join(re.findall('title:\s(.*?),', data))
+            articleid = ''.join(re.findall('articleid:\s(.*?),', data))
+            channel = ''.join(re.findall('channel:\s(.*?),', data))
+            cid = ''.join(re.findall('cid:\s(.*?),', data))
+            commentNumber = ''.join(re.findall('title:\s(.*?),', data))
+            pass
+        except Exception as e:
+            self.logger.error('parse_content {}, {}, {}'.format(e, response.url, response.text))
